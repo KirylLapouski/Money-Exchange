d8b6516b7ac49dbe5762338b85760d769ca4084a
KirylLapouski <lapkovskyk@mail.ru>
2018-09-29 12:42:48 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,64 @@
 // PLEASE DON'T change function name
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+   return  compose(
+        checkToRich,
+        pennies,
+        nickels,
+        dimes,
+        quarters,
+        halfDollars,
+        checkNegative
+    )(currency)
+}
+function halfDollars(currency){
+    let res = {}
+    if(Math.floor(currency/50))
+        res['H'] = Math.floor(currency/50)
+    
+    return [currency%50,res]
+}
+
+function quarters([currency,res]){
+    let quarters = {}
+    if(Math.floor(currency/25))
+        quarters['Q'] = Math.floor(currency/25)
+
+    return [currency%25, {...res, ...quarters}]
+}
+
+function dimes([currency,res]){
+    let dimes = {}
+    if(Math.floor(currency/10))
+        dimes['D'] = Math.floor(currency/10)
+
+    return [currency%10, {...res, ...dimes}]
+}
+
+
+function nickels([currency,res]){
+    let nickels = {}
+    if(Math.floor(currency/5))
+        nickels['N'] = Math.floor(currency/5)
+
+    return [currency%5, {...res, ...nickels}]
+}
+
+function pennies([currency,res]){
+    let nickels = {}
+    if(Math.floor(currency/1))
+        nickels['P'] = Math.floor(currency/1)
+
+    return [currency%1, {...res, ...nickels}]
+}
+
+function checkToRich([currency,res]){
+    if(res.H >= 200)
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    return res
+}
+function checkNegative(currency){
+    if(currency<0)
+        return 0
+    return currency
 }
